ff6070949f2270ebb968763f457b585314f3abe9
piyup0198 <70134549+piyup0198@users.noreply.github.com>
2022-12-09 17:47:29 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,54 @@
+package test.training;
+import java.util.Scanner;
+
+public class BankAccount {
+
+     private String accno;
+     private String name;
+     private long balance;
+
+     Scanner sc = new Scanner(System.in);
+
+     //method to open an account
+    void openAccount() {
+        System.out.print("Enter Account No: ");
+        accno = sc.next();
+        System.out.print("Enter Name: ");
+        name = sc.next();
+        System.out.print("Enter Balance: ");
+        balance = sc.nextLong();
+    }
+
+    //method to display account details
+    void showAccount() {
+
+        System.out.println(accno + "," + name + "," + balance);
+                            }
+
+    //method to deposit money
+    void deposit() {
+        long amount;
+        System.out.println("Enter Amount U Want to Deposit : ");
+        amount = sc.nextLong();
+        balance = balance + amount;
+    }
+
+    //method to withdraw money
+    void withdrawal() {
+        long amount;
+        System.out.println("Enter Amount U Want to withdraw : ");
+        amount = sc.nextLong();
+        if (balance >= amount) {
+            balance = balance - amount;
+        } else {
+            System.out.println("Less Balance..Transaction Failed..");
+        }
+    }
+    boolean search(String acno) {
+        if (accno.equals(acno)) {
+            showAccount();
+            return (true);
+        }
+        return (false);
+    }
+}
